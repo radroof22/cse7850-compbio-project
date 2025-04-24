ef3c4b6382ad88ee7360ef78cea09bafa67fe5a4
Sanjay Sai Mamidipaka <smamidipaka3@atl1-1-03-017-2-0.pace.gatech.edu>
2025-04-24 13:18:52 -0400

    added eval files

--- eval_proteinclip_pp.py
+++ eval_proteinclip_pp.py
@@ -11,7 +11,7 @@ options = ort.SessionOptions()
 options.intra_op_num_threads = 2
 
 # Load model with options
-session = ort.InferenceSession("./proteinclip/project_1.onnx", sess_options=options)
+session = ort.InferenceSession("./proteinclip/project3d_1.onnx", sess_options=options)
 
 # Get input details
 df = pd.read_parquet("./proteinclip/protclip_embed_dataset.parquet")
@@ -19,8 +19,18 @@ df = df[2000:2100]
 print('length of input embedding', len(df['proteinclip_embed'].iloc[0]))
 
 input_name = session.get_inputs()[0].name
-input_array = np.stack(df["proteinclip_embed"].to_numpy()).astype(np.float32)
+# input_array = np.stack(df["proteinclip_embed"].to_numpy()).astype(np.float32)
+
 # print(f"Input shape: {input_array.shape}")  # Should be (100, embed_dim)
+
+with open("./data/structural/structural_embeddings_2000_5000.pkl", "rb") as f:
+    embeddings_data = pickle.load(f)
+
+# Convert to DataFrame (assuming the embeddings are stored in a dictionary-like structure)
+df = pd.DataFrame(embeddings_data)
+df = df[0:100]
+input_array = np.stack(df["embedding"].to_numpy()).astype(np.float32)
+
 
 # Run inference
 outputs = session.run(None, {input_name: input_array})
@@ -40,13 +50,13 @@ output_umap = umap_model.fit_transform(output_array)
 plot_df = pd.DataFrame({
     "x": np.concatenate([input_umap[:, 0], output_umap[:, 0]]),
     "y": np.concatenate([input_umap[:, 1], output_umap[:, 1]]),
-    "Type": ["ProteinClip"] * len(input_umap) + ["ProteinClip+"] * len(output_umap)
+    "Type": ["ProteinClip"] * len(input_umap) + ["ProteinClip++"] * len(output_umap)
 })
 
 # Plot
 plt.figure(figsize=(10, 6))
 sns.scatterplot(data=plot_df, x="x", y="y", hue="Type", alpha=0.7, palette=["#1f77b4", "#ff7f0e"])
-plt.title("UMAP Projection of Embeddings (ProteinClip vs. ProteinClip+)")
+plt.title("UMAP Projection of Embeddings (ProteinClip vs. ProteinClip++)")
 plt.xlabel("UMAP-1")
 plt.ylabel("UMAP-2")
 plt.grid(True)
